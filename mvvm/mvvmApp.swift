bbc4277d20a6c3fbae2fa6b72c54216294f947f7
ismv00 <ismv00@icloud.com>
2023-01-13 22:16:10 -0400

    Initial Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Igor S. Menezes on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
